1bb431eea541bbcef0806dad570ca074777df463
erokemwa <erokemwa@yahoo.com>
2023-04-24 12:30:51 -0700

    Dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,30 +1,53 @@
 #!/usr/bin/python3
-"""Module to fetch to-do list information for"""
+
+"""Fetches JSON data from an API"""
+
 import json
 import requests
 
-if __name__ == '__main__':
-    # Define the API base URL
-    base_url = "https://jsonplaceholder.typicode.com"
+# Check if the current module is the main program
+if __name__ == "__main__":
 
-    # Get the list of users and todos from the API
-    users = requests.get(f"{base_url}/users").json()
-    todos = requests.get(f"{base_url}/todos").json()
+    # Define the user URL for the API
+    user_url = "https://jsonplaceholder.typicode.com/users/"
 
-    # Create a dictionary to store the todos for each user
-    user_todos = {}
-    for user in users:
-        user_id = user.get('id')
-        user_todos[user_id] = []
-        for todo in todos:
-            if user_id == todo.get('userId'):
-                user_todos[user_id].append({
-                    'task': todo.get('title'),
-                    'completed': todo.get('completed'),
-                    'username': user.get('username')
-                })
+    # Get JSON data from the API and store it in a dictionary
+    user_dict = requests.get(user_url).json()
+
+    # Define the output file name
+    file_name = "todo_all_employees.json"
+
+    # Initialize an empty dictionary
+    _dict = {}
+
+    # Iterate over each user in the dictionary
+    for elem in user_dict:
+
+        # Get the user's name and ID
+        name = elem.get("username")
+        user_id = str(elem.get("id"))
+
+        # Get the user's to-do list from the API
+        user_data = requests.get("{}{}/todos".format(user_url, user_id))
+        user_data = user_data.json()
+
+        # Initialize an empty list for the user's to-do items
+        _dict[user_id] = []
+
+        # Iterate over each to-do item for the user
+        for item in user_data:
+
+            # Create a dictionary for each to-do item
+            inner_dict = {}
+
+            # Add the username, task, and completion status to the dictionary
+            inner_dict["username"] = name
+            inner_dict["task"] = item.get("title")
+            inner_dict["completed"] = item.get("completed")
+
+            # Add the dictionary to the user's to-do list
+            _dict[user_id].append(inner_dict)
 
     # Write the dictionary to a JSON file
-    filename = "todo_all_employees.json"
-    with open(filename, "w", encoding="utf-8") as json_file:
-        json.dump(user_todos, json_file)
+    with open(file_name, 'w') as f:
+        json.dump(my_dict, f)
